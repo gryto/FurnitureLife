87b99f2e0b8d3da644f4f350d730d25b37f67de9
solihahnur <57170706+gryto@users.noreply.github.com>
2022-06-26 20:54:37 +0700

    connect to mysql
    Signed-off-by: solihahnur <57170706+gryto@users.noreply.github.com>

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['user'])|$checkrole!=1){
 
     <div class="jumbroton jumbroton-fluid text-center">
         <div class="container">
-            <h1 class="dispaly-4"> halo Selamat Datang! </h1>
+            <h1 class="display-4"> Halo, Selamat Datang! </h1>
             </h1>
         </div>
     </div>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+//Menghubungkan database
+$conn = mysqli_connect('localhost', 'ani', 'Harrypotter123@', 'furniturelife');
+
+//mengecek koneksi
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+//Membuat query untuk semua produk
+
+
 ?>
 
 <!DOCTYPE html>
